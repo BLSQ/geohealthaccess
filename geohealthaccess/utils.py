da2c94b71a5713774ee2437245bd75449ca0a8e7
Yann Forget <yannforget@mailbox.org>
2019-09-26 15:46:01 +0200

    Add function to download file from a public FTP server

--- geohealthaccess/utils.py
+++ geohealthaccess/utils.py
@@ -1,10 +1,12 @@
 import json
 import os
+from ftplib import FTP
 from pkg_resources import resource_string
+from urllib.parse import urlparse
 
 import requests
 from shapely.geometry import shape
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 
 def country_geometry(country):
@@ -67,4 +69,48 @@ def download_from_url(session, url, output_dir, show_progress=True,
                         progress_bar.update(1024)
         if show_progress:
             progress_bar.close()
+    return local_path
+
+
+def download_from_ftp(url, output_dir, overwrite=False):
+    """Download a file from a public FTP server.
+
+    Parameters
+    ----------
+    url : str
+        Path to remote file (ftp://<ftp_server>/<dir>/<file>).
+    output_dir : str
+        Path to local output directory.
+    overwrite : bool, optional
+        Overwrite local file.
+    
+    Returns
+    -------
+    local_path : str
+        Local path to downloaded file.
+    """
+    url = urlparse(url)
+    ftp = FTP(url.netloc)
+    ftp.login()
+    parts = url.path.split('/')
+    filename = parts[-1]
+    directory = '/'.join(parts[-2]) + '/'
+    ftp.cwd(directory)
+    filesize = ftp.size(filename)
+    progress = tqdm(total=filesize, desc=filename, unit_scale=True, unit='B')
+    local_path = os.path.join(output_dir, filename)
+
+    with open(local_path, 'wb') as f:
+
+        def write_and_progress(chunk):
+            """Custom callback function that write data chunk to disk
+            and update the progress bar accordingly.
+            """
+            f.write(chunk)
+            progress.update(len(chunk))
+        
+        ftp.retrbinary(f'RETR {filename}', write_and_progress)
+    
+    progress.close()
+    ftp.close()
     return local_path
