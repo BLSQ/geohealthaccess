b9f1b8ced2d9accbf4b2f34eb63e3527a0b7aae9
Yann Forget <yannforget@mailbox.org>
2020-01-19 19:04:18 +0100

    Add accessibility script

--- geohealthaccess/modeling.py
+++ geohealthaccess/modeling.py
@@ -6,6 +6,7 @@ from pkg_resources import resource_filename
 
 import numpy as np
 import rasterio
+from rasterio.crs import CRS
 from rasterio.features import rasterize
 import geopandas as gpd
 
@@ -52,25 +53,25 @@ def get_segment_speed(highway, tracktype=None, smoothness=None, surface=None,
     return base_speed * min(tracktype, smoothness, surface)
 
 
-def rasterize_road_network(src_data, dst_filename, crs, transform,
-                           width, height, network_speeds=None):
+def speed_from_roads(src_filename, dst_filename, dst_transform, dst_crs,
+                     dst_width, dst_height, network_speeds=None):
     """Convert network geometries to a raster with cell values equal
     to speed in km/h.
 
     Parameters
     ----------
-    src_data : str
+    src_filename : str
         Path to input network geometries (with the following columns: geometry,
         highway, smoothness, tracktype and surface).
     dst_filename : str
         Path to output raster.
-    crs : dict
-        CRS of the output raster.
-    transform : Affine
+    dst_transform : Affine
         Affine transform of the output raster.
-    width : int
+    dst_crs : dict
+        CRS of the output raster.
+    dst_width : int
         Output raster width.
-    height : int
+    dst_height : int
         Output raster height.
     network_speeds : dict, optional
         Speeds associated to each OSM tag. If not provided,
@@ -81,8 +82,11 @@ def rasterize_road_network(src_data, dst_filename, crs, transform,
     dst_filename : str
         Path to output raster.
     """
-    network = gpd.read_file(src_data)
+    network = gpd.read_file(src_filename)
     network = network[network.geom_type == 'LineString']
+    network.crs = CRS.from_epsg(4326)
+    if network.crs != dst_crs:
+        network = network.to_crs(crs)
 
     shapes = []
     for _, row in network.iterrows():
@@ -93,8 +97,8 @@ def rasterize_road_network(src_data, dst_filename, crs, transform,
 
     speed_raster = rasterize(
         shapes=shapes,
-        out_shape=(height, width),
-        transform=transform,
+        out_shape=(dst_height, dst_width),
+        transform=dst_transform,
         fill=0,
         all_touched=True,
         dtype=rasterio.dtypes.uint8)
@@ -102,15 +106,125 @@ def rasterize_road_network(src_data, dst_filename, crs, transform,
     dst_profile = rasterio.profiles.DefaultGTiffProfile()
     dst_profile.update(
         count=1,
-        crs=crs,
-        width=width,
-        height=height,
-        transform=transform,
+        crs=dst_crs,
+        width=dst_width,
+        height=dst_height,
+        transform=dst_transform,
         dtype=rasterio.dtypes.uint8,
-        nodata=255)
+        nodata=255,
+        tiled=True,
+        blockxsize=256,
+        blockysize=256,
+        compress='LZW')
 
     with rasterio.open(dst_filename, 'w', **dst_profile) as dst:
         dst.write(speed_raster, 1)
+    return dst_filename
+
+
+def speed_from_landcover(src_filename, dst_filename, water_filename,
+                         landcover_speeds=None):
+    """Assign speed to each pixel in km/h based on the proportion
+    of each land cover class in the cell. Each land cover class
+    has a predefined speed value provided in the `landcover_speeds`
+    dictionnary. 
+
+    Parameters
+    ----------
+    src_filename : str
+        Path to input land cover raster (multiband raster with one band
+        per class, band descriptions with land cover label, and pixel
+        values corresponding to land cover percentages.
+    dst_filename : str
+        Path to output raster.
+    water_filename : str
+        Path to surface water raster.
+    landcover_speeds : dict, optional
+        Speeds associated to each land cover category. If not provided,
+        default values will be used.
+    
+    Returns
+    -------
+    dst_filename : str
+        Path to output raster.
+    """
+    with rasterio.open(src_filename) as src:
+        dst_profile = src.profile
+        dst_profile.update(
+            count=1,
+            dtype=np.float32,
+            nodata=-1)
+
+    # Load default land cover speeds if not provided
+    if not landcover_speeds:
+        with open(resource_filename(__name__, 'resources/land-cover.json')) as f:
+            landcover_speeds = json.load(f)
+
+    with rasterio.open(dst_filename, 'w', **dst_profile) as dst, \
+         rasterio.open(water_filename) as src_water, \
+         rasterio.open(src_filename) as src_land:
+        for ij, window in dst.block_windows(1):
+            speed = np.zeros(shape=(window.height, window.width),
+                             dtype=np.float32)
+            for id, landcover in enumerate(src_land.descriptions, start=1):
+                print(landcover)
+                coverfraction = src_land.read(window=window, indexes=id)
+                speed += (coverfraction / 100) * landcover_speeds[landcover]
+            surface_water = src_water.read(window=window, indexes=1)
+            speed[surface_water >= 2] = 0
+            dst.write(speed, window=window, indexes=1)
+
+    return dst_filename
+
+
+def combine_speed_rasters(landcover_speed, roadnetwork_speed, dst_filename):
+    """Combine land cover and road network speed rasters into a single GeoTIFF
+    by keeping the max. speed value between both rasters.
+
+    Parameters
+    ----------
+    landcover_speed : str
+        Path to land cover speed raster.
+    roadnetwork_speed : str
+        Path to road network speed raster.
+    dst_filename : str
+        Path to output raster.
+    
+    Returns
+    -------
+    dst_filename : str
+        Path to output raster.
+    """
+    with rasterio.open(landcover_speed) as src:
+        dst_profile = src.profile
+    with rasterio.open(landcover_speed) as src_land, \
+         rasterio.open(roadnetwork_speed) as src_road, \
+         rasterio.open(dst_filename, 'w', **dst_profile) as dst:
+        for ij, window in dst.block_windows(1):
+            speed = np.maximum(src_land.read(window=window, indexes=1),
+                               src_road.read(window=window, indexes=1))
+            dst.write(speed, window=window, indexes=1)
+    return dst_filename
+
+
+def compute_friction(speed_raster, dst_filename, max_time=3600):
+    """Convert speed raster to friction, i.e. time to cross a given pixel."""
+    with rasterio.open(speed_raster) as src:
+        dst_profile = src.profile
+        xres, yres = abs(src.transform.a), abs(src.transform.e)
+        dst_profile.update(dtype=np.float64)
+    with rasterio.open(speed_raster) as src, \
+         rasterio.open(dst_filename, 'w', **dst_profile) as dst:
+        for ij, window in dst.block_windows(1):
+            speed = src.read(window=window, indexes=1).astype(np.float64)
+            speed /= 3.6  # From km/hour to m/second
+            diag_distance = np.sqrt(xres * xres + yres * yres)
+            time_to_cross = distance / speed
+            # Clean bad values
+            time_to_cross[speed == 0] = max_time
+            time_to_cross[np.isinf(time_to_cross)] = max_time
+            time_to_cross[time_to_cross > max_time] = max_time
+            dst.write(time_to_cross, window=window, indexes=1)
     return dst_filename
 
 
@@ -168,3 +282,81 @@ def land_cover_speed(src_datadir, dst_filename, crs, transform,
     with rasterio.open(dst_filename, 'w', **dst_profile) as dst:
         dst.write(speed_raster, 1)
     return dst_filename
+
+
+def add_surface_water(landcover_speed, surface_water, dst_file):
+    """Use surface water raster to update land cover speeds.
+
+    Parameters
+    ----------
+    landcover_speed : str
+        Path to land cover speed raster.
+    surface_water : str
+        Path to surface water raster, with pixel values
+        equal to the number of months with water cover.
+    dst_file : str
+        Path to output raster.
+    
+    Returns
+    -------
+    dst_file : str
+        Path to output raster.
+    """
+    pass
+
+
+def combine_speeds(landcover, roadnetwork, dst_file):
+    """Combine land cover and road network speeds into
+    a single raster by keeping the max speed for each cell.
+
+    Parameters
+    ----------
+    landcover : str
+        Path to land cover speed raster.
+    roadnetwork : str
+        Path to road network speed raster.
+    water : str
+        Path to surface water raster.
+    dst_file : str
+        Output raster path.
+    
+    Returns
+    -------
+    dst_file : str
+        Path to output raster.
+    """
+    with rasterio.open(landcover) as src:
+        dst_profile = src.profile
+    # Use windowed read/write to save memory
+    with rasterio.open(dst_file, 'w', **dst_profile) as dst:
+        with rasterio.open(landcover) as src_landcover:
+            for ji, window in src.block_windows(1):
+                land_speed = src_landcover.read(1, window=window)
+                with rasterio.open(roadnetwork) as src_road:
+                    road_speed = src_road.read(1, window=window)
+                dst.write(np.maximum(land_speed, road_speed),
+                          window=window, indexes=1)
+    return dst_file
+
+
+def set_water_as_obstacle(speed, water, dst_file):
+    """Assign null speed (obstacle) to surface water cells.
+
+    Parameters
+    ----------
+    speed : str
+        Path to speed raster.
+    water : str
+        Path to surface water raster.
+    dst_file : str
+        Path to output raster.
+    
+    Returns
+    -------
+    dst_file : str
+        Path to output raster.
+    """
+    with rasterio.open(speed) as src:
+        dst_profile = src.profile
+    # Use windowed read/write to save memory
+    pass
---
+++ geohealthaccess/scripts/accessibility.py
@@ -0,0 +1,169 @@
+"""Compute per-cell travel times."""
+
+
+import argparse
+import json
+import os
+
+import geopandas as gpd
+import rasterio
+from rasterio.crs import CRS
+from rasterio.features import rasterize
+import whitebox
+
+from geohealthaccess import modeling
+from geohealthaccess.config import load_config
+
+
+def compute_friction(input_dir, interm_dir, landcover_speeds, network_speeds):
+    """Assign per-cell speed from land cover and road network. Then compute
+    friction raster from speed.
+
+    Parameters
+    ----------
+    input_dir : str
+        Path to input data directory (raw data).
+    interm_dir : str
+        Path to intermediary data directory (preprocessed data).
+    landcover_speeds : dict
+        Speed associated with each land cover category.
+    network_speeds : dict
+        Speed and adjustment factors associated with road
+        types and properties.
+    
+    Returns
+    -------
+    friction : str
+        Path to output raster.
+    """
+    # Assign per-cell speed based on land cover and surface water
+    landcover_speed = modeling.speed_from_landcover(
+        src_filename=os.path.join(interm_dir, 'landcover.tif'),
+        dst_filename=os.path.join(interm_dir, 'landcover_speed.tif'),
+        water_filename=os.path.join(interm_dir, 'surface_water.tif'),
+        landcover_speeds=landcover_speeds)
+    
+    # Assign per-cell speed based on roads and paths
+    with rasterio.open(landcover_speed) as src:
+        dst_transform = src.transform
+        dst_crs = src.crs,
+        dst_width = src.width,
+        dst_height = src.height
+    osm_dir = os.path.join(input_dir, 'openstreetmap')
+    osm_datafile = [f for f in os.listdir(osm_dir)
+                    if f.endswith('.gpkg')][0]
+    road_speed = modeling.speed_from_roads(
+        src_filename=os.path.join(osm_dir, osm_datafile),
+        dst_filename=os.path.join(interm_dir, 'road_speed.tif'),
+        dst_transform=dst_transform,
+        dst_crs=dst_crs,
+        dst_width=dst_width,
+        dst_height=dst_height,
+        network_speeds=network_speeds)
+    
+    # Combine both speed rasters by keeping max. speed value
+    speed = os.path.join(interm_dir, 'speed.tif')
+    modeling.combine_speed_rasters(landcover_speed, road_speed, speed) 
+
+    # Friction raster, i.e. time to cross each pixel
+    friction = os.path.join(interm_dir, 'friction.tif')
+    modeling.compute_friction(speed, friction, max_time=3600)
+
+    return friction
+
+
+def rasterize_points(points, dst_filename, dst_transform, dst_crs,
+                     dst_height, dst_width):
+    """Rasterize a GeoDataFrame of points."""
+    if not points.crs:
+        points.crs = CRS.from_epsg(4326)
+    if dst_crs != points.crs:
+        points = points.to_crs(dst_crs)
+
+    raster = rasterize(
+        shapes=[g.__geo_interface__ for g in points.geometry],
+        transform=dst_transform,
+        out_shape=(dst_height, dst_width),
+        all_touched=True,
+        default_value=1,
+        fill=0)
+    
+    profile = rasterio.default_gtiff_profile
+    profile.update(
+        count=1,
+        nodata=255,
+        transform=dst_transform,
+        crs=dst_crs,
+        width=dst_width,
+        height=dst_height)
+
+    with rasterio.open(dst_filename, 'w', **profile) as dst:
+        dst.write(raster, 1)
+    
+    return dst_filename
+
+
+def compute_traveltime(destinations, friction, dst_dir, label):
+    """TODO."""
+    out_accum = os.path.join(dst_dir, f'accumulated_cost_{label}.tif')
+    out_backlink = os.path.join(dst_dir, f'backlink_{label}.tif')
+    wbt = whitebox.WhiteboxTools()
+    wbt.cost_distance(
+        source=destinations,
+        cost=friction,
+        out_accum=out_accum,
+        out_backlink=out_backlink)
+    return out_accum
+
+
+def main():
+    # Parse command-line arguments & load configuration
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_file',
+                        type=str,
+                        help='.ini configuration file')
+    args = parser.parse_args()
+    conf = load_config(args.config_file)
+    interm_dir = conf['DIRECTORIES']['IntermDir']
+
+    # Load speed values as dict
+    with open(conf['MODELING']['LandCoverSpeeds']) as f:
+        landcover_speeds = json.load(f)
+    with open(conf['MODELING']['RoadNetworkSpeeds']) as f:
+        network_speeds = json.load(f)
+
+    # Run script
+    friction = compute_friction(
+        input_dir=conf['DIRECTORIES']['InputDir'],
+        interm_dir=interm_dir,
+        landcover_speeds=landcover_speeds,
+        network_speeds=network_speeds)
+
+    with rasterio.open(friction) as src:
+        dst_transform, dst_crs = src.transform, src.crs
+        dst_width, dst_height = src.width, src.height
+
+    for label, path in conf['DESTINATIONS'].items():
+
+        points = gpd.read_file(path)
+        points_raster = os.path.join(interm_dir, f'points_{label}.tif')
+
+        rasterize_points(
+            points=points,
+            dst_filename=points_raster,
+            dst_transform=dst_transform,
+            dst_crs=dst_crs,
+            dst_height=dst_height,
+            dst_width=dst_width)
+
+        compute_traveltime(
+            destinations=points_raster,
+            friction=friction,
+            dst_dir=conf['DIRECTORIES']['OutputDir'],
+            label=label)
+    
+    return
+
+
+if __name__ == '__main__':
+    main()
