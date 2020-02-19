9f6d26656657210df1af25a547be1f3645ce4227
Yann Forget <yannforget@mailbox.org>
2020-02-19 13:48:04 +0100

    Fix. Gitlab Pages CI

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,8 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+module_dir = os.path.join(os.path.dirname(__name__), '..')
+sys.path.insert(0, os.path.abspath(module_dir))
 
 
 # -- Project information -----------------------------------------------------
