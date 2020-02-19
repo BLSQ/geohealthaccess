f78e5469698c9c43a17446ac7b30a91c7d337880
Yann Forget <yannforget@mailbox.org>
2020-02-19 13:59:33 +0100

    Fix. Gitlab Pages CI

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,19 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autodoc_mock_imports = [
+        'requests',
+        'gdal',
+        'shapely',
+        'pandas',
+        'geopandas',
+        'beautifulsoup4',
+        'tqdm',
+        'rasterio',
+        'appdirs',
+        'whitebox'
+]
+
 
 # -- Options for HTML output -------------------------------------------------
 
