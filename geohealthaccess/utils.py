28efc30904e089552438889874d433b2d1e248a2
Yann Forget <yannforget@mailbox.org>
2020-06-05 19:35:41 +0200

    Add logging and fix overwriting

--- geohealthaccess/utils.py
+++ geohealthaccess/utils.py
@@ -1,5 +1,7 @@
 import json
 import logging
+from time import ctime
+from datetime import datetime
 import os
 from ftplib import FTP, error_reply
 from pkg_resources import resource_string
@@ -21,6 +23,46 @@ def human_readable_size(size, decimals=1):
             break
         size /= 1000
     return f'{size:.{decimals}f} {unit}'
+
+
+def http_same_size(url, fname, session=None):
+    """Compare remote and local sizes using the Content-Length
+    HTTP header.
+    """
+    if not os.path.isfile(fname):
+        return False
+    headers = {'Accept-Encoding': 'identity'}
+    if session:
+        r = session.head(url, allow_redirects=True, headers=headers)
+    else:
+        r = requests.head(url, allow_redirects=True, headers=headers)
+    content_length = int(r.headers.get('Content-Length'))
+    local_size = os.path.getsize(fname)
+    return content_length == local_size
+
+
+def http_newer(url, fname, session=None):
+    """Compare Last-Modified HTTP header and file metadata to
+    check for changes.
+    """
+    if not os.path.isfile(fname):
+        return True
+    if session:
+        r = session.head(url, allow_redirects=True)
+    else:
+        r = requests.head(url, allow_redirects=True)
+
+    mtime_http = datetime.strptime(
+        r.headers.get('Last-Modified'),
+        '%a, %d %b %Y %H:%M:%S %Z'
+    )
+
+    mtime_local = datetime.strptime(
+        ctime(os.path.getmtime(fname)),
+        '%a %b %d %H:%M:%S %Y'
+    )
+    
+    return mtime_http > mtime_local
 
 
 def country_geometry(country):
@@ -55,8 +97,7 @@ def download_from_url(session, url, output_dir, show_progress=True,
     show_progress : bool, optional (default=True)
         Show a progress bar.
     overwrite : bool, optional (default=False)
-        If set to `False`, local files will not be overwritten if they have
-        the same size as the remote file.
+        If set to `True`, output file will be removed before download.
 
     Returns
     -------
@@ -66,23 +107,43 @@ def download_from_url(session, url, output_dir, show_progress=True,
     os.makedirs(output_dir, exist_ok=True)
     filename = url.split('/')[-1]
     local_path = os.path.join(output_dir, filename)
+
     with session.get(url, stream=True, timeout=5) as r:
-        r.raise_for_status()
-        file_size = int(r.headers['Content-Length'])
-        if os.path.isfile(local_path) and not overwrite:
-            if os.path.getsize(local_path) == file_size:
-                return local_path
+
+        try:
+            r.raise_for_status()
+        except Exception as e:
+            log.error(e)
+        
+        # Remove old file if overwrite
+        if os.path.isfile(local_path) and overwrite:
+            log.info(f'Removing old {filename} file.')
+            os.remove(local_path)
+
+        # Skip download if remote and local sizes are equal
+        if http_same_size(url, local_path, session):
+            log.info(f'Remote and local sizes of {filename} are equal. Skipping download.')
+            return local_path
+
+        # Setup progress bar
         if show_progress:
+            size = int(r.headers.get('Content-Length'))
             progress_bar = tqdm(
-                desc=filename, total=file_size, unit_scale=True, unit='B')
+                desc=filename, total=size, unit_scale=True, unit='B')
+
         with open(local_path, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
                     if show_progress:
                         progress_bar.update(1024)
+
         if show_progress:
             progress_bar.close()
+    
+    filesize = human_readable_size(os.path.getsize(local_path))
+    log.info(f'Downloaded file {filename} ({filesize}).')
+
     return local_path
 
 
@@ -132,16 +193,21 @@ def download_from_ftp(url, output_dir, overwrite=False):
     file_size = ftp.size(filename)
     local_path = os.path.join(output_dir, filename)
 
+    # Remove old file if overwrite
+    if os.path.isfile(local_path) and overwrite:
+        log.info(f'File {filename} already exists. Removing it.')
+        os.remove(local_path)
+    
     # Do not download again if local and remote file sizes are equal
     if os.path.isfile(local_path):
         if os.path.getsize(local_path) == file_size:
-            log.info(f'File {local_path} already exists. Skipping download.')
+            log.info(f'File {filename} already exists. Skipping download.')
             return local_path
         else:
-            log.info(f'File {local_path} already exists but its size differ. Removing old file.')
+            log.info(f'File {filename} already exists but size differs. Removing old file.')
             os.remove(local_path)
 
-    log.info(f'Downloading file {url} to {local_path}.')
+    log.info(f'Downloading {url} to {local_path}.')
     progress = tqdm(total=file_size, desc=filename, unit_scale=True, unit='B')
 
     with open(local_path, 'wb') as f:
@@ -156,7 +222,7 @@ def download_from_ftp(url, output_dir, overwrite=False):
         ftp.retrbinary(f'RETR {filename}', write_and_progress)
     
     size = human_readable_size(os.path.getsize(local_path))
-    log.info(f'Downloaded file {local_path} ({size}).')
+    log.info(f'Downloaded {filename} ({size}).')
     progress.close()
     ftp.close()
     return local_path
