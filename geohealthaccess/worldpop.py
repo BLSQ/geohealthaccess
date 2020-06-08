cd4669d555d7ec8376141815f5ff9ae205276b45
Yann Forget <yannforget@mailbox.org>
2020-06-08 18:16:05 +0200

    Support overwrites and updates in worldpop download

--- geohealthaccess/worldpop.py
+++ geohealthaccess/worldpop.py
@@ -2,6 +2,8 @@
 
 import logging
 import os
+from collections import namedtuple
+from ftplib import FTP
 
 from geohealthaccess.utils import download_from_ftp
 
@@ -9,6 +11,22 @@ log = logging.getLogger(__name__)
 
 FTP_HOST = "ftp.worldpop.org.uk"
 BASE_DIR = "GIS/Population/Global_2000_2020"
+
+
+def list_available_years(country):
+    """List available years for Worldpop population data for a given
+    country identified by its 3-letter country code.
+    """
+    years = []
+    ftp = FTP(FTP_HOST)
+    ftp.login()
+    listdir = ftp.nlst("GIS/Population/Global_2000_2020/")
+    for path in listdir:
+        fname = path.split("/")[-1]
+        if fname.isnumeric() and len(fname) == 4:
+            years.append(int(fname))
+    return years
+    ftp.close()
 
 
 def build_url(country, year):
@@ -31,24 +49,83 @@ def build_url(country, year):
     return f"ftp://{FTP_HOST}/{directory}/{filename}"
 
 
-def download(country, year, output_dir, overwrite=False):
-    """Download WorldPop 100m population data for a given
-    year and country.
+def download(country, output_dir, year=None, overwrite=False):
+    """Download WorldPop 100m population data for a given country.
+    Automatically get latest data if year is not specified.
 
     Parameters
     ----------
     country : str
         Country ISO A3 code (example: 'COD').
-    year : int
-        Year of interest (between 2000 and 2020).
+    output_dir : str
+        Path to output directory.
+    year : int, optional
+        Year of interest (between 2000 and 2020). If not specified,
+        latest year available is used.
+    overwrite : bool, optional
+        Force overwrite of existing data.
     
     Returns
     -------
     local_path : str
         Path to downloaded file.
     """
+    if not year:
+        available_years = list_available_years(country)
+        year = max(available_years)
+        log.info(f"No year specified. Selected latest year available ({year}).")
     url = build_url(country, year)
     log.info(f"Downloading worldpop data from URL {url}.")
-    local_path = download_from_ftp(url, output_dir)
+    local_path = download_from_ftp(url, output_dir, overwrite=overwrite)
     log.info(f"Downloaded worldpop data to {os.path.abspath(local_path)}.")
     return local_path
+
+
+def _parse_worldpop_filename(filename):
+    """Parse worldpop filename into a namedtuple."""
+    WorldpopFile = namedtuple(
+        "WorldpopFile", ["country", "datatype", "prefix", "year", "suffix", "filename"]
+    )
+    basename = filename.split(".")[0]
+    if "UNadj" in basename:
+        country, datatype, year, suffix = basename.split("_")
+    else:
+        country, datatype, year = basename.split("_")
+        suffix = ""
+    prefix = f"{country}_{datatype}"
+    return WorldpopFile(country, datatype, prefix, int(year), suffix, filename)
+
+
+def _clean_datadir(directory):
+    """Check a directory for multiple Worldpop data files and keep only the
+    latest one.
+    """
+    rasters = [f for f in os.listdir(directory) if f.lower().endswith(".tif")]
+    if len(rasters) <= 1:
+        log.info(f"Data directory does not require cleaning.")
+        return
+
+    # Make a summary of all years available for each prefix/suffix combination
+    summary = {}
+    datafiles = [_parse_worldpop_filename(f) for f in rasters]
+    for datafile in datafiles:
+        key = "__".join([datafile.prefix, datafile.suffix])
+        if key not in summary:
+            summary[key] = []
+        summary[key].append(datafile.year)
+
+    # Create a list of files to keep (one per prefix-suffix combination)
+    to_keep = []
+    for key, years in summary.items():
+        prefix, suffix = key.split("__")
+        latest = max(years)
+        if suffix:
+            to_keep.append(f"{prefix}_{latest}_{suffix}.tif")
+        else:
+            to_keep.append(f"{prefix}_{latest}.tif")
+
+    for raster in rasters:
+        if raster not in to_keep:
+            log.info(f"Removing {raster} because a more recent version is available.")
+            os.remove(os.path.join(directory, raster))
+    return
--- tests/test_worldpop.py
+++ tests/test_worldpop.py
@@ -1,12 +1,89 @@
+import os
+import tempfile
+
 import pytest
 
 from geohealthaccess import worldpop
 
 
-@pytest.mark.parametrize('country, year, expected_url', [
-    ('COD', 2015, 'ftp://ftp.worldpop.org.uk/GIS/Population/Global_2000_2020/2015/COD/cod_ppp_2015.tif'),
-    ('COd', 2010, 'ftp://ftp.worldpop.org.uk/GIS/Population/Global_2000_2020/2010/COD/cod_ppp_2010.tif'),
-    ('mdg', 2018, 'ftp://ftp.worldpop.org.uk/GIS/Population/Global_2000_2020/2018/MDG/mdg_ppp_2018.tif')
-])
+@pytest.mark.parametrize(
+    "country, year, expected_url",
+    [
+        (
+            "COD",
+            2015,
+            "ftp://ftp.worldpop.org.uk/GIS/Population/Global_2000_2020/2015/COD/cod_ppp_2015.tif",
+        ),
+        (
+            "COd",
+            2010,
+            "ftp://ftp.worldpop.org.uk/GIS/Population/Global_2000_2020/2010/COD/cod_ppp_2010.tif",
+        ),
+        (
+            "mdg",
+            2018,
+            "ftp://ftp.worldpop.org.uk/GIS/Population/Global_2000_2020/2018/MDG/mdg_ppp_2018.tif",
+        ),
+    ],
+)
 def test_build_url(country, year, expected_url):
     assert worldpop.build_url(country, year) == expected_url
+
+
+def test_list_available_years():
+    available_years = worldpop.list_available_years("TON")
+    assert len(available_years) >= 20
+    assert 2000 in available_years
+
+
+def test__parse_worldpop_filename():
+    FNAME = "ton_ppp_2016_UNadj.tif"
+    datafile = worldpop._parse_worldpop_filename(FNAME)
+    assert datafile.country == "ton"
+    assert datafile.datatype == "ppp"
+    assert datafile.prefix == "ton_ppp"
+    assert datafile.year == 2016
+    assert datafile.suffix == "UNadj"
+
+
+def test__clean_datadir():
+    # Create empty files in a temporary directory
+    RASTERS = [
+        "ton_ppp_2015.tif",
+        "ton_ppp_2016_UNadj.tif",
+        "ton_ppp_2018.tif",
+        "mdg_ppp_2015.tif",
+        "mdg_ppp_2017.tif",
+    ]
+    with tempfile.TemporaryDirectory() as tmpdir:
+
+        # create empty files
+        for raster in RASTERS:
+            open(os.path.join(tmpdir, raster), "a").close()
+
+        worldpop._clean_datadir(tmpdir)
+        remaining = os.listdir(tmpdir)
+
+        expected = ["ton_ppp_2016_UNadj.tif", "ton_ppp_2018.tif", "mdg_ppp_2017.tif"]
+        assert set(remaining) == set(expected)
+
+
+def test_download():
+    COUNTRY = "TON"  # Tonga
+    with tempfile.TemporaryDirectory() as tmpdir:
+
+        # Simple download
+        fname = worldpop.download(COUNTRY, tmpdir)
+        available_years = worldpop.list_available_years(COUNTRY)
+        latest = max(available_years)
+        assert os.path.isfile(fname)
+        assert str(latest) in os.path.basename(fname)
+        mtime = os.path.getmtime(fname)
+
+        # Should not be downloaded again
+        worldpop.download(COUNTRY, tmpdir, overwrite=False)
+        assert os.path.getmtime(fname) == mtime
+
+        # Should be downloaded again
+        worldpop.download(COUNTRY, tmpdir, overwrite=True)
+        assert os.path.getmtime(fname) != mtime
