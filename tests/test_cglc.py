4c40b3e6b9d933076a9b3a2350ba63c9c48aaa12
Yann <yannforget@mailbox.org>
2021-03-19 16:39:33 +0100

    Add tests for new CGLC module

--- tests/test_cglc.py
+++ tests/test_cglc.py
@@ -1,128 +1,131 @@
-"""Tests for CGLC module."""
+"""Tests for cglc module."""
 
 import os
+from glob import glob
 from tempfile import TemporaryDirectory
 
+import numpy as np
 import pytest
-import vcr
+import rasterio
+import requests
+from pkg_resources import resource_filename
+from rasterio.crs import CRS
+from shapely import wkt
 
-from geohealthaccess.cglc import (
-    CGLC,
-    _is_cglc,
-    find_layer,
-    list_layers,
-    parse_filename,
-    unique_tiles,
-)
+from geohealthaccess import cglc
 
 
-@vcr.use_cassette("tests/cassettes/cglc-manifest.yaml")
-def test_parse_manifest():
-    cglc = CGLC()
-    assert len(cglc.manifest) == 94
-    assert all([url.startswith("https://") for url in cglc.manifest])
+@pytest.fixture(scope="module")
+def catalog():
+    return cglc.CGLC()
 
 
-@vcr.use_cassette("tests/cassettes/cglc-manifest.yaml")
-def test_spatial_index():
-    cglc = CGLC()
-    assert len(cglc.sindex) == 94
-    assert type(cglc.sindex.index[0]) == str
-    assert cglc.sindex.unary_union.bounds == (-180, -60, 180, 80)
-
-
-@vcr.use_cassette("tests/cassettes/cglc-manifest.yaml")
-def test_cglc_search(senegal, madagascar):
-    cglc = CGLC()
-    assert sorted(cglc.search(senegal)) == ["W020N20"]
-    assert sorted(cglc.search(madagascar)) == ["E040N00", "E040S20"]
-
-
-@vcr.use_cassette("tests/cassettes/cglc-W180N40.yaml")
-def test_cglc_download():
-    cglc = CGLC()
-    with TemporaryDirectory(prefix="geohealthaccess_") as tmpdir:
-        fpath = cglc.download("W180N40", tmpdir, overwrite=False)
-        mtime = os.path.getmtime(fpath)
-        assert os.path.isfile(fpath)
-        # tile should not be downloaded again
-        cglc.download("W180N40", tmpdir, overwrite=False)
-        assert os.path.getmtime(fpath) == mtime
-        # tile should be downloaded again
-        cglc.download("W180N40", tmpdir, overwrite=True)
-        assert os.path.getmtime(fpath) != mtime
-
-
-@vcr.use_cassette("tests/cassettes/cglc-W180N40-head.yaml")
-def test_download_size():
-    cglc = CGLC()
-    assert cglc.download_size("E040N00") == 360011771
-
-
-def test_parse_filename():
-    FNAME = (
-        "E040N00_ProbaV_LC100_epoch2015_global_v2.0.1"
-        "_grass-coverfraction-StdDev_EPSG-4326.tif"
+def test_download_url(catalog):
+    url = catalog.download_url("E000N00", "BuiltUp", 2019)
+    assert url == (
+        "https://s3-eu-west-1.amazonaws.com/vito.landcover.global/v3.0.1/2019/E000N00/"
+        "E000N00_PROBAV_LC100_global_v3.0.1_2019-nrt_"
+        "BuiltUp-CoverFraction-layer_EPSG-4326.tif"
     )
-    layer = parse_filename(FNAME)
-    assert layer.name == "grass-coverfraction-StdDev"
-    assert layer.tile == "E040N00"
-    assert layer.epoch == 2015
-    assert layer.version == "v2.0.1"
 
-
-def test_parse_filename_errors():
     with pytest.raises(ValueError):
-        parse_filename("random_filename.tif")
+        catalog.download_url("E000N00", "NotALabel", 2019)
+
+    with pytest.raises(ValueError):
+        catalog.download_url("E000N00", "BuiltUp", 2000)
 
 
-def test_is_cglc():
-    FNAME1 = (
-        "E040N00_ProbaV_LC100_epoch2015_global_v2.0.1"
-        "_grass-coverfraction-StdDev_EPSG-4326.tif"
-    )
-    FNAME2 = (
-        "E040N00_ProbaV_LC100_epoch2015" "_grass-coverfraction-StdDev_EPSG-4326.tif"
-    )
-    assert _is_cglc(FNAME1)
-    assert not _is_cglc(FNAME2)
+def test_format_latlon(catalog):
+    assert catalog.format_latlon(20, 30) == "E030N20"
+    assert catalog.format_latlon(-40, -120) == "W120S40"
 
 
-FNAMES = [
-    "E040N00_ProbaV_LC100_epoch2015_global_v2.0.1_crops-coverfraction-StdDev_EPSG-4326.tif",
-    "E040N00_ProbaV_LC100_epoch2015_global_v2.0.1_crops-coverfraction-layer_EPSG-4326.tif",
-    "E040S20_ProbaV_LC100_epoch2015_global_v2.0.1_grass-coverfraction-layer_EPSG-4326.tif",
-    "E040S20_ProbaV_LC100_epoch2015_global_v2.0.1_grass-coverfraction-StdDev_EPSG-4326.tif",
-]
+def test_search(catalog):
+    with open(resource_filename(__name__, "data/madagascar.wkt")) as f:
+        geom = wkt.load(f)
+        tiles = sorted(catalog.search(geom))
+        expected = sorted(["E040N00", "E040S20"])
+        assert tiles == expected
+    with open(resource_filename(__name__, "data/senegal.wkt")) as f:
+        geom = wkt.load(f)
+        tiles = sorted(catalog.search(geom))
+        expected = sorted(["W020N20"])
+        assert tiles == expected
 
 
-def test_unique_tiles():
-    # create temporary dir with empty files named after FNAMES
-    with TemporaryDirectory(prefix="geohealthaccess_") as tmpdir:
-        for fname in FNAMES:
-            open(os.path.join(tmpdir, fname), "a").close()
-        unique = unique_tiles(tmpdir)
-        assert len(unique) == 2
-        assert "E040N00" in unique and "E040S20" in unique
+def test_download(catalog, monkeypatch):
+    tile = "W020N20"
+
+    # mock requests iter_content method so that file content
+    # is not downloaded.
+    # nb: a header request is still sent.
+    def mockreturn(self, chunk_size):
+        return [b"", b"", b""]
+
+    monkeypatch.setattr(requests.Response, "iter_content", mockreturn)
+
+    with TemporaryDirectory() as tmpdir:
+        f = catalog.download(
+            tile=tile,
+            label="BuiltUp",
+            output_dir=tmpdir,
+            year=2019,
+            show_progress=False,
+        )
+        f = os.path.basename(f)
+        assert f.startswith(tile) and f.endswith(".tif")
+        assert "BuiltUp" in f and "2019" in f
 
 
-def test_list_layers():
-    # create temporary dir with empty files named after FNAMES
-    with TemporaryDirectory(prefix="geohealthaccess_") as tmpdir:
-        for fname in FNAMES:
-            open(os.path.join(tmpdir, fname), "a").close()
-        layernames = list_layers(tmpdir, "E040S20")
-        print(layernames)
-        assert len(layernames) == 2
-        assert "grass-coverfraction-layer" in layernames
-        assert "grass-coverfraction-StdDev" in layernames
+def test_download_all(catalog, monkeypatch):
+    tile = "W020N20"
+
+    # mock requests iter_content method so that file content
+    # is not downloaded.
+    # nb: a header request is still sent.
+    def mockreturn(self, chunk_size):
+        return [b"", b"", b""]
+
+    monkeypatch.setattr(requests.Response, "iter_content", mockreturn)
+
+    with TemporaryDirectory() as tmpdir:
+        catalog.download_all(
+            tile=tile,
+            output_dir=tmpdir,
+            year=2019,
+            show_progress=False,
+        )
+        for label in catalog.LABELS:
+            assert glob(os.path.join(tmpdir, f"*{label}*.tif"))
 
 
-def test_find_layer():
-    # create temporary dir with empty files named after FNAMES
-    with TemporaryDirectory(prefix="geohealthaccess_") as tmpdir:
-        for fname in FNAMES:
-            open(os.path.join(tmpdir, fname), "a").close()
-        layerpath = find_layer(tmpdir, "E040S20", "grass-coverfraction-layer")
-        assert os.path.isfile(layerpath)
-        assert "grass-coverfraction-layer" in os.path.basename(layerpath)
+def test_preprocess(catalog):
+    # use geometry from D.R. Congo
+    with open(resource_filename(__name__, "data/cod.wkt")) as f:
+        geom = wkt.load(f)
+    with TemporaryDirectory() as tmpdir:
+        # reproject to 10km pixel sizes for faster processing
+        cglc.preprocess(
+            input_dir=os.path.join(resource_filename(__name__, "data/cglc")),
+            dst_dir=tmpdir,
+            geom=geom,
+            crs=CRS.from_epsg(3857),
+            res=10000,
+            overwrite=False,
+        )
+        for label in catalog.LABELS:
+            # check that raster exists
+            ls = glob(os.path.join(tmpdir, f"*{label}*.tif"))
+            assert ls
+            with rasterio.open(ls[0]) as src:
+                # check raster metadata
+                assert src.crs == CRS.from_epsg(3857)
+                assert src.nodata == -9999
+                assert src.dtypes[0] == "float32"
+                data = src.read(1, masked=True)
+                # values should be between 0 and 100 as they are percentages
+                assert data.min() >= 0
+                assert data.max() <= 100
+                # at least 25% of non-null pixels
+                h, w = data.shape
+                assert np.count_nonzero(data) / (h * w) >= 0.25
