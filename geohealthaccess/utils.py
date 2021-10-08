0ca019599425f068268c9529cfd062f55c66ccc9
Yann Forget <yannforget@mailbox.org>
2021-10-08 15:20:53 +0200

    Increase default timeout for HTTP download

--- geohealthaccess/utils.py
+++ geohealthaccess/utils.py
@@ -84,7 +84,13 @@ def country_geometry(country):
 
 
 def download_from_url(
-    session, url, output_dir, show_progress=True, overwrite=False, pbar_position=0
+    session,
+    url,
+    output_dir,
+    show_progress=True,
+    overwrite=False,
+    pbar_position=0,
+    timeout=30,
 ):
     """Download remote file from URL in a given requests session.
 
@@ -103,6 +109,8 @@ def download_from_url(
     pbar_position : int, optional (default=0)
         Optionally set the absolute position of the progress bar in case several
         threads display a progress bar simultaneously.
+    timeout : int, optional (default=30)
+        GET request timeout in seconds.
 
     Returns
     -------
@@ -113,7 +121,7 @@ def download_from_url(
     filename = url.split("/")[-1]
     dst_file = os.path.join(output_dir, filename)
 
-    with session.get(url, stream=True, timeout=5) as r:
+    with session.get(url, stream=True, timeout=timeout) as r:
 
         try:
             r.raise_for_status()
